aed7fecc7257b065c82ce228201625db2f3af7a6
tryandaiman <tryanda01@gmail.com>
2020-08-03 14:37:48 +0700

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import 'assets/scss/style.scss/'
+
+function App() {
+  return (
+    <div className="App">
+     Hai Dunia!
+    </div>
+  );
+}
+
+export default App;
